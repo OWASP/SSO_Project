13c715797690f5587b7ae6869111efd6b4e3e26c
JamesCullum <JamesCullum@users.noreply.github.com>
2020-04-12 19:55:23 +0200

    Wait for database only in dev environment

--- js-backend/entrypoint.sh
+++ js-backend/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Waiting on database to be ready"
-/app/wait-for-it.sh database:3306 -t 600
+echo "Checking environment"
+CHECKIP=$(getent hosts database)
+if [ -z "$CHECKIP" ]; then
+	echo "Production environment detected, not waiting on database"
+else
+	echo "Dev environment detected, waiting on database"
+	/app/wait-for-it.sh database:3306 -t 600
+fi
 
 echo "Starting test"
 npm run serve
